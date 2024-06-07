1f45183cf2d43f275ab67c5715a59295b7aa490e
FelipeB1007 <102627296+FelipeB1007@users.noreply.github.com>
2024-06-07 08:01:10 -0300

    commit

--- script.js
+++ script.js
@@ -6,15 +6,15 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas =  [
     {
-    enunciado: "Pergunta 1",
-    alternativas: [
-        "Alternativa 1",
-        "Alternativa 2"
-    ]
-}
-{
-    enunciado: "Pergunta 1",
-    alternativas: [
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
             "Alternativa 1",
             "Alternativa 2"
         ]
